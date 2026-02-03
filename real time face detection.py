f7e98d5d4adcf1d9a9a844f7c0bb7f428d7ae54f
darshan13-sd <darshansenthil13092007@gmail.com>
2026-02-03 18:21:29 +0530

    Update real time face detection.py

--- real time face detection.py
+++ real time face detection.py
@@ -1,32 +1,4 @@
-# import cv2
-# import numpy as np
 
-# #https://github.com/Itseez/opencv/tree/master/data/haarcascades
-# face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-# #pic= cv2.imread('beskham-jpg')
-# videocapture=cv2.VideoCapture(0)
-# scale_factor= 1.3
-
-# while 1:
-#     ret,pic =videocapture.read()
-#     faces = face_cascade.detectMultiScale(pic, scale_factor, 5)
-
-#     for (x, y, w, h) in faces:
-
-#         cv2.rectangle(pic, (x, y), (x+w, y+h), (255, 0, 0), 2)
-
-#         font = cv2.FONT_HERSHEY_SIMPLEX
-
-#         cv2.putText(pic, 'Beckham', (x, y), font, 2, (255, 255, 255), 2, cv2.LINE_AA)
-
-#     print("number of faces found)".format(len(faces)))
-
-#     cv2.imshow('face', pic)
-#     k = cv2.waitKey(30) & 0xff
-#     if k-27:
-#         break
-# cv2.destroyAllWindows()
 import cv2
 import numpy as np
 
@@ -73,3 +45,4 @@ while True:
 
 videocapture.release()
 cv2.destroyAllWindows()
+
